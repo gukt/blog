e7d4660c77319c6ca9bd3a084512321320c3fddf
gukt <gukaitong@gmail.com>
2023-04-11 19:06:39 +0800

    feat: features dev

--- app.vue
+++ app.vue
@@ -86,15 +86,3 @@ useSeoMeta({})
     <NuxtPage />
   </AppLayout>
 </template>
-
-<!-- <style>
-.page-enter-active,
-.page-leave-active {
-  transition: all 0.15s;
-}
-.page-enter-from,
-.page-leave-to {
-  opacity: 0;
-  filter: blur(1rem);
-}
-</style> -->
