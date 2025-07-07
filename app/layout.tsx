11965e4d8ea5bed0c3565c32fbddbda1bb2a7d0a
gukt <29283212@qq.com>
2025-07-07 23:42:49 +0800

    import Footer from "./ui/footer";

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "@/app/ui/globals.css";
-import Footer from "@/app/ui/footer";
+// import Footer from "@/app/ui/footer";
+import Footer from "./ui/footer";
 import HeaderWrapper from "@/app/ui/header-wrapper";
 
 export const metadata: Metadata = {
